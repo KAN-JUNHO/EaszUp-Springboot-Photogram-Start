83d1008207875ea8d13f7763acbfd97a17e9728b
junho <junho6667@gmail.com>
2021-08-11 17:26:33 +0900

    2021/08/11

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,12 +1,13 @@
 // (1) 회원정보 수정
-function update(userid) {
+function update(userId) {
+	
 	let data=$("#profileUpdate").serialize();
 	
 	console.log(data);
 	
 	$.ajax({
 		type:"put",
-		url:"/api/user/${userid}",
+		url:`/api/user/${userId}`,
 		data:data,
 		contentType:"application/x-www-form-urlencoded; charset=utf-8",
 		dataType:"json"
